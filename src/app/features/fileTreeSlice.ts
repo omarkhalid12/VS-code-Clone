c64f41a0c88f24b5ce781945c32d652c75f3b442
omarkhalid12 <ok3817554@gmail.com>
2024-04-15 18:45:44 +0300

    File Tree Redux Slice

---
+++ src/app/features/fileTreeSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+const fileTreeSlice = createSlice({
+  name: "fileTree",
+  initialState,
+  reducers: {}
+})
+export default fileTreeSlice.reducer
