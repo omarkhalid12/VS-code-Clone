bd058704c404865ec17d39b45ea394563da4f169
omarkhalid12 <ok3817554@gmail.com>
2024-04-15 18:21:20 +0300

    Refactoing File Icons

--- src/components/RenderFileIcons.tsx
+++ src/components/RenderFileIcons.tsx
@@ -1,3 +1,4 @@
+import { extensionIconPaths } from "../constant"
 import IconImg from "./IconImg"
 import FileIcon from "./SVG/File"
 
@@ -6,39 +7,18 @@ interface IProps {
   isFolder?: boolean
   isOpen?: boolean
 }
+
 const RenderFileIcons = ({ fileName, isFolder, isOpen } : IProps) => {
   const extension = fileName.split(".").pop()
+  if(extension && Object.prototype.hasOwnProperty.call(extensionIconPaths, extension)) {
+    const iconPath = isFolder ? 
+    isOpen ? 
+    `${extensionIconPaths[extension]}-open.svg` 
+    : `${extensionIconPaths[extension]}.svg` 
+    : `${extensionIconPaths[extension]}.svg`
 
-  //** Files ..
-  if(extension === "tsx") return <IconImg src="/icons/react_ts.svg" />
-  if(extension === "js") return <IconImg src="/icons/javascript.svg" />
-  if(extension === "jsx") return <IconImg src="/icons/react.svg" />
-  if(extension === "html") return <IconImg src="/icons/html.svg" />
-
-  //** Folders ..
-  if(extension === "node_modules" && isFolder) 
-    return isOpen ? (
-      <IconImg src="/icons/folder-node-open.svg" /> ) : ( 
-      <IconImg src="/icons/folder-node.svg" /> 
-    )
-
-  if(extension === "public" && isFolder) 
-    return isOpen ? (
-      <IconImg src="/icons/folder-public-open.svg" /> ) : ( 
-      <IconImg src="/icons/folder-public.svg" /> 
-    )
-
-  if(extension === "src" && isFolder) 
-    return isOpen ? (
-      <IconImg src="/icons/folder-src-open.svg" /> ) : ( 
-      <IconImg src="/icons/folder-src.svg" /> 
-    )
-
-  if(extension === "components" && isFolder) 
-    return isOpen ? (
-      <IconImg src="/icons/folder-components-open.svg" /> ) : ( 
-      <IconImg src="/icons/folder-components.svg" /> 
-    )
+    return <IconImg src={iconPath}/>
+  }
 
   if(isFolder && isOpen) return <IconImg src="/icons/folder-default-open.svg"/>
   if(isFolder && !isOpen) return <IconImg src="/icons/folder-default.svg"/>
