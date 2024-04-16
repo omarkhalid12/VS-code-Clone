95237f07a4410f83ec524a0766c47e0dd3f31c20
omarkhalid12 <ok3817554@gmail.com>
2024-04-16 20:57:41 +0300

    Remove Tab Handler

--- src/components/OpenedFilesBarTab.tsx
+++ src/components/OpenedFilesBarTab.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { IFile } from "../interfaces"
 import RenderFileIcons from "./RenderFileIcons"
 import CloseIcon from "./SVG/CloseIcon"
-import { setClickedFileAction } from "../app/features/fileTreeSlice"
+import { setClickedFileAction, setOpenedFileAction } from "../app/features/fileTreeSlice"
 import { RootState } from "../app/store"
 
 interface IProps {
@@ -10,13 +10,19 @@ interface IProps {
 }
 const OpenedFilesBarTab = ({ file } : IProps) => {
   const dispatch = useDispatch()
-  const {clickedFile: {activeTabId}} = useSelector((state: RootState)=> state.tree)
+  const {openedFile, clickedFile: {activeTabId}} = useSelector((state: RootState)=> state.tree)
 
   //** Handlers ..
   const onClick = ()=> {
     const {id, name, content} = file;
     dispatch(setClickedFileAction({filename: name, fileContent: content,
       activeTabId: id}));
+  }
+  const onRemove = (selectedId: string)=> {
+    const filtered = openedFile.filter(file => file.id !== selectedId)
+    const { id, name, content} = filtered[filtered.length - 1]
+    dispatch(setOpenedFileAction(filtered))
+    dispatch(setClickedFileAction({activeTabId: id,filename: name, fileContent: content}))
   }
   
   return (
@@ -28,7 +34,12 @@ const OpenedFilesBarTab = ({ file } : IProps) => {
         <span className="flex items-center justify-center p-1 mx-2 duration-300 rounded-md cursor-pointer w-fit">
           <li>{file.name}</li>
         </span>
-        <span className="flex items-center justify-center p-1 mr-2 duration-300 rounded-md cursor-pointer w-fit">
+        <span className="flex items-center justify-center p-1 mr-2 duration-300 rounded-md cursor-pointer w-fit"
+        onClick={e => {
+          e.stopPropagation();
+          onRemove(file.id)
+        }}
+        >
           <CloseIcon />
         </span>
     </div>
